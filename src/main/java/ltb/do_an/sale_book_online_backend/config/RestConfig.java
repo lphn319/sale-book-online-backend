c587b0bef6f95d86ab03107ddb827d40c76c4ac3
lphn319 <linhpht263@outlook.com.vn>
2024-10-13 06:56:50 +0700

    Fix controller đăng kí tài khoản

--- src/main/java/ltb/do_an/sale_book_online_backend/config/RestConfig.java
+++ src/main/java/ltb/do_an/sale_book_online_backend/config/RestConfig.java
@@ -16,38 +16,38 @@ public class RestConfig implements RepositoryRestConfigurer {
     private String url = "http://localhost:3000";
     @Autowired
     private EntityManager entityManager;
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
         // expose ids
         // Cho phép trả về id
         config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream().map(Type::getJavaType).toArray(Class[]::new));
-        // config.exposeIdsFor(TheLoai.class);
-
         //CORS configuaration
         cors.addMapping("/**").allowedOrigins(url).allowedMethods("GET", "POST", "PUT", "DELETE");
 
         // Chặn các methods
-        HttpMethod[] chanCacPhuongThuc ={
-                HttpMethod.POST,
-                HttpMethod.PUT,
-                HttpMethod.PATCH,
-                HttpMethod.DELETE,
-        };
+//        HttpMethod[] chanCacPhuongThuc = {
+//                HttpMethod.POST,
+//                HttpMethod.PUT,
+//                HttpMethod.PATCH,
+//                HttpMethod.DELETE,
+//        };
 //        disableHttpMethods(TheLoai.class, config, chanCacPhuongThuc);
 
-        // Chặn các method DELETE
-        HttpMethod[] phuongThucDelete = {
-                HttpMethod.DELETE
-        };
-//        disableHttpMethods(NguoiDung.class, config,phuongThucDelete );
-    }
-
-    private void disableHttpMethods(Class c,
-                                    RepositoryRestConfiguration config,
-                                    HttpMethod[] methods){
-        config.getExposureConfiguration()
-                .forDomainType(c)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(methods))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(methods));
+//        // Chặn các method DELETE
+//        HttpMethod[] phuongThucDelete = {
+//                HttpMethod.DELETE
+//        };
+////        disableHttpMethods(NguoiDung.class, config,phuongThucDelete );
+//    }
+//
+//    private void disableHttpMethods(Class c,
+//                                    RepositoryRestConfiguration config,
+//                                    HttpMethod[] methods){
+//        config.getExposureConfiguration()
+//                .forDomainType(c)
+//                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(methods))
+//                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(methods));
+//    }
     }
 }
--- src/main/java/ltb/do_an/sale_book_online_backend/controller/TaiKhoanController.java
+++ src/main/java/ltb/do_an/sale_book_online_backend/controller/TaiKhoanController.java
@@ -5,10 +5,7 @@ import ltb.do_an.sale_book_online_backend.service.TaiKhoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tai-khoan")
@@ -17,6 +14,7 @@ public class TaiKhoanController {
     @Autowired
     private TaiKhoanService taiKhoanService;
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/dang-ky")
     public ResponseEntity<?> DangKyNguoiDung(@Validated @RequestBody NguoiDung nguoiDung) {
         ResponseEntity<?> response = taiKhoanService.DangKyNguoiDung(nguoiDung);
